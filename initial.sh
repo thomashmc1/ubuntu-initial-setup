91cac55fbe0aa67dfb8d1c8efde53e1cef742641
Thomas Chan <thomashmc1@gmail.com>
2016-09-14 23:22:10 -0400

    Create initial.sh

---
+++ initial.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#	Install Unity and VNCServer
+
+sudo apt-get update
+
+echo "Install Git..."
+sudo apt-get install git
+echo "Git installed ******"
+
+echo "Install NVM..."
+yes Y | sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
+echo "NVM installed ******"
+
+echo "Install Unity GUI..."
+yes Y | sudo apt-get install ubuntu-desktop
+echo "Unity Installed *****"
+
+
+echo "Install VNVServer"
+yes Y | sudo apt-get install tightvncserver
+echo "VNVServer Installed *****"
