b3780f79ed641fcdebeedf614cde125f576c25f1
Thomas Chan <thomashmc1@gmail.com>
2016-09-14 23:53:02 -0400

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -4,18 +4,18 @@
 sudo apt-get update
 
 echo "Install Git..."
-sudo apt-get -y install git
+sudo apt-get install git -y --force-yes 
 echo "Git installed ******"
 
-echo "Install NVM..."
-sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
-echo "NVM installed ******"
-
 echo "Install Unity GUI..."
-sudo apt-get -y install ubuntu-desktop
+sudo apt-get install xubuntu-desktop xfce4 firefox nano -y --force-yes
 echo "Unity Installed *****"
 
 
 echo "Install VNVServer"
-sudo apt-get -y install tightvncserver
+sudo apt-get -y install tightvncserver -y --force-yes 
 echo "VNVServer Installed *****"
+
+echo "Install NVM..."
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.7/install.sh | bash
+echo "NVM installed ******"
